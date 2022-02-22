9d58c2b2ec8abad3eb9fd73ff950769a8921c8ed
Kai Johnson <kai.johnson@tonyelumelufoundation.org>
2022-02-22 08:59:08 +0000

    Async test program

---
+++ csharp/Await/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class AwaitOperator
+{
+    public static async Task Main()
+    {
+        Task<int> downloading = DownloadDocsMainPageAsync();
+        Console.WriteLine($"{nameof(Main)}: Launched downloading.");
+
+        int bytesLoaded = await downloading;
+        Console.WriteLine($"{nameof(Main)}: Downloaded {bytesLoaded} bytes.");
+    }
+
+    private static async Task<int> DownloadDocsMainPageAsync()
+    {
+        Console.WriteLine($"{nameof(DownloadDocsMainPageAsync)}: About to start downloading.");
+
+        var client = new HttpClient();
+        Thread.Sleep(900);
+        // All of this up to here executes before giving control back to Main()
+        byte[] content = await client.GetByteArrayAsync("https://docs.microsoft.com/en-us/");
+
+        Console.WriteLine($"{nameof(DownloadDocsMainPageAsync)}: Finished downloading.");
+        return content.Length;
+    }
+}
